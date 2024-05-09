f6fc5c75bdb3fd0789a6b0e951550019d78bf0bc
sangameshsoppimat <sangameshbusines@gmail.com>
2024-05-09 23:20:01 +0530

    fh

--- src/test/java/JenPrcts/login.java
+++ src/test/java/JenPrcts/login.java
@@ -2,6 +2,8 @@ package JenPrcts;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,11 +11,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class login 
 {
 	WebDriver driver;
+	@Parameters("Browser")
 	@Test
-	public void loh() throws InterruptedException
+	public void loh(String BrowserName) throws InterruptedException
 	{
-		WebDriverManager.chromedriver().setup();
-		driver=new ChromeDriver();
+		if(BrowserName.equalsIgnoreCase("Chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}
+		else if(BrowserName.equalsIgnoreCase("FireFox"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver=new FirefoxDriver();
+		}
+		
+		
 		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 		Thread.sleep(2000);
 	}
