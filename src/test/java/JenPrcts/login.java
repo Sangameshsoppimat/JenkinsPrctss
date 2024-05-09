20316b13ae61728525e8d5be6c5d3cb490350c84
sangameshsoppimat <sangameshbusines@gmail.com>
2024-05-09 22:41:55 +0530

    sgf

---
+++ src/test/java/JenPrcts/login.java
@@ -0,0 +1,21 @@
+package JenPrcts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class login 
+{
+	WebDriver driver;
+	@Test
+	public void loh() throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(2000);
+	}
+
+}
